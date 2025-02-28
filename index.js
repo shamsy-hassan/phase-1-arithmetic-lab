9b22a7e19cc17a3050badfe9d553865a3d7cafca
shamsy-hassan <shamsyhassan254@gmail.com>
2025-02-28 22:39:40 +0300

    assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let numbers = [3, 10, 6, 20, 15];
+let max = Math.max(...numbers);
+
+console.log(max);
 
 // Write your code here
